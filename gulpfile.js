cabab146c678b0b781e1fabd8ec3c113b24e3ace
Елизавета <e.bagrintseva@gmail.com>
2021-03-18 14:32:22 +0300

    Исправляет ошибки, кроме npm i

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ gulp.task('html', function () {
 gulp.task('copy', function () {
   return gulp.src([
     'source/fonts/**/*.{woff,woff2}',
-    'source/js/**',
+    'source/js/*.js',
     'source/img/*.ico',
   ], {
     base: 'source'
