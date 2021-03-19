dffe83a878d73b7821988d998cbf474e7af8f3c8
Елизавета <e.bagrintseva@gmail.com>
2021-03-19 16:45:52 +0300

    Update 2021-03-19T13:45:48.682Z

--- js/main.js
+++ js/main.js
@@ -4,41 +4,41 @@
 
   const body = document.querySelector('body');
   const close = document.querySelector('.close');
-  const modal = document.querySelector('.modal');
-  const name = document.querySelector('.modal').querySelector('input[type="text"]');
   const open = document.querySelector('.open');
   const wrapper = document.querySelector('.modal-wrapper');
 
-  if (open) {
-    open.addEventListener('click', function (evt) {
+  open.addEventListener('click', function (evt) {
+    if (document.querySelector('.modal')) {
+      const name = document.querySelector('.modal').querySelector('input[type="text"]');
       evt.preventDefault();
-      modal.classList.add('modal--open');
+      document.querySelector('.modal').classList.add('modal--open');
       wrapper.classList.add('modal-wrapper--active');
       name.focus();
       body.classList.add('body--no-scroll');
-    });
+    }
+  });
 
-    const closeModal = function () {
-      modal.classList.remove('modal--open');
-      wrapper.classList.remove('modal-wrapper--active');
-      body.classList.remove('body--no-scroll');
-    };
+  const closeModal = function () {
+    document.querySelector('.modal').classList.remove('modal--open');
+    wrapper.classList.remove('modal-wrapper--active');
+    body.classList.remove('body--no-scroll');
+  };
 
-    close.addEventListener('click', closeModal);
-    wrapper.addEventListener('click', closeModal);
+  close.addEventListener('click', closeModal);
+  wrapper.addEventListener('click', closeModal);
 
-    window.addEventListener('keydown', function (evt) {
-      if (evt.key === 'Escape') {
-        closeModal();
-      }
-    });
-  }
+  window.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      closeModal();
+    }
+  });
+
 
   const phones = document.querySelectorAll('input[type="tel"]');
 
   if (phones) {
     for (let i = 0; i < phones.length; i++) {
-      IMask(phones[i], { mask: '+{7}(000)000-00-00' });
+      new window.IMask(phones[i], { mask: '+{7}(000)000-00-00' });
     }
   }
 
